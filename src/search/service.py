e8df4ef7e79077808e5d9004254e438168a58eac
Tobias Alexander Wulff <tobias.wulff@live.de>
2025-09-24 16:56:32 +0200

    add a more complete SEE (Static Exchange Evaluation) and integrated it into move pruning

--- src/search/service.py
+++ src/search/service.py
@@ -96,6 +96,217 @@ class SearchService:
                 and a.promotion == b.promotion
             )
 
+        # --- SEE helpers ---
+        def piece_on_square(sq: int) -> Optional[int]:
+            for p in range(12):
+                if (board.bb[p] >> sq) & 1:
+                    return p
+            return None
+
+        def attackers_to_square(sq: int, occ: int, by_white: bool, removed_mask: int) -> int:
+            attackers = 0
+            f = sq % 8
+            r = sq // 8
+            # Pawns
+            if by_white:
+                if f > 0:
+                    o = sq - 9
+                    if o >= 0 and ((board.bb[WP] >> o) & 1) and not ((removed_mask >> o) & 1):
+                        attackers |= 1 << o
+                if f < 7:
+                    o = sq - 7
+                    if o >= 0 and ((board.bb[WP] >> o) & 1) and not ((removed_mask >> o) & 1):
+                        attackers |= 1 << o
+            else:
+                if f < 7:
+                    o = sq + 9
+                    if o <= 63 and ((board.bb[BP] >> o) & 1) and not ((removed_mask >> o) & 1):
+                        attackers |= 1 << o
+                if f > 0:
+                    o = sq + 7
+                    if o <= 63 and ((board.bb[BP] >> o) & 1) and not ((removed_mask >> o) & 1):
+                        attackers |= 1 << o
+            # Knights
+            deltas = [(-1, 2), (1, 2), (-2, 1), (2, 1), (-2, -1), (2, -1), (-1, -2), (1, -2)]
+            for df, dr in deltas:
+                tf = f + df
+                tr = r + dr
+                if 0 <= tf < 8 and 0 <= tr < 8:
+                    o = tr * 8 + tf
+                    if by_white:
+                        if ((board.bb[WN] >> o) & 1) and not ((removed_mask >> o) & 1):
+                            attackers |= 1 << o
+                    else:
+                        if ((board.bb[BN] >> o) & 1) and not ((removed_mask >> o) & 1):
+                            attackers |= 1 << o
+            # King
+            kdeltas = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
+            for df, dr in kdeltas:
+                tf = f + df
+                tr = r + dr
+                if 0 <= tf < 8 and 0 <= tr < 8:
+                    o = tr * 8 + tf
+                    if by_white:
+                        if ((board.bb[WK] >> o) & 1) and not ((removed_mask >> o) & 1):
+                            attackers |= 1 << o
+                    else:
+                        if ((board.bb[BK] >> o) & 1) and not ((removed_mask >> o) & 1):
+                            attackers |= 1 << o
+
+            # Sliders: bishop-like and rook-like
+            def ray_dirs(diagonals: bool) -> List[Tuple[int, int]]:
+                return (
+                    [(-1, -1), (1, -1), (-1, 1), (1, 1)]
+                    if diagonals
+                    else [(-1, 0), (1, 0), (0, -1), (0, 1)]
+                )
+
+            # Bishop/queen
+            for df, dr in ray_dirs(True):
+                tf, tr = f, r
+                while True:
+                    tf += df
+                    tr += dr
+                    if not (0 <= tf < 8 and 0 <= tr < 8):
+                        break
+                    o = tr * 8 + tf
+                    if (occ >> o) & 1:
+                        if by_white:
+                            if ((board.bb[WB] >> o) & 1 or (board.bb[WQ] >> o) & 1) and not (
+                                (removed_mask >> o) & 1
+                            ):
+                                attackers |= 1 << o
+                        else:
+                            if ((board.bb[BB] >> o) & 1 or (board.bb[BQ] >> o) & 1) and not (
+                                (removed_mask >> o) & 1
+                            ):
+                                attackers |= 1 << o
+                        break
+            # Rook/queen
+            for df, dr in ray_dirs(False):
+                tf, tr = f, r
+                while True:
+                    tf += df
+                    tr += dr
+                    if not (0 <= tf < 8 and 0 <= tr < 8):
+                        break
+                    o = tr * 8 + tf
+                    if (occ >> o) & 1:
+                        if by_white:
+                            if ((board.bb[WR] >> o) & 1 or (board.bb[WQ] >> o) & 1) and not (
+                                (removed_mask >> o) & 1
+                            ):
+                                attackers |= 1 << o
+                        else:
+                            if ((board.bb[BR] >> o) & 1 or (board.bb[BQ] >> o) & 1) and not (
+                                (removed_mask >> o) & 1
+                            ):
+                                attackers |= 1 << o
+                        break
+            return attackers
+
+        def see(move: Move) -> int:
+            # Only intended for captures
+            to_sq = move.to_sq
+            from_sq = move.from_sq
+            side_white = board.side_to_move == "w"
+            # Occupancy
+            occ = 0
+            for b in board.bb:
+                occ |= b
+            # Identify victim
+            if board.ep_square is not None and move.to_sq == board.ep_square:
+                # En passant: victim is pawn behind the target square
+                victim_sq = to_sq - 8 if side_white else to_sq + 8
+                victim_piece = BP if side_white else WP
+            else:
+                victim_sq = to_sq
+                vp = piece_on_square(victim_sq)
+                if vp is None:
+                    return 0
+                victim_piece = vp
+            victim_value = 100
+            piece_vals = [
+                100,
+                320,
+                330,
+                500,
+                900,
+                20000,
+                100,
+                320,
+                330,
+                500,
+                900,
+                20000,
+            ]
+            victim_value = piece_vals[victim_piece]
+
+            # Determine initial attacker piece type (handle promotion)
+            if side_white:
+                if move.promotion:
+                    promo_map = {"q": WQ, "r": WR, "b": WB, "n": WN}
+                    attacker_piece = promo_map.get(move.promotion, WP)
+                else:
+                    attacker_piece = piece_on_square(from_sq)
+            else:
+                if move.promotion:
+                    promo_map = {"q": BQ, "r": BR, "b": BB, "n": BN}
+                    attacker_piece = promo_map.get(move.promotion, BP)
+                else:
+                    attacker_piece = piece_on_square(from_sq)
+            if attacker_piece is None:
+                return 0
+
+            gain: List[int] = [victim_value]
+
+            # Update occupancy for first capture
+            removed_mask = 0
+            # Remove victim from board
+            occ &= ~(1 << victim_sq)
+            removed_mask |= 1 << victim_sq
+            # Remove attacker from origin and occupy target
+            occ &= ~(1 << from_sq)
+            removed_mask |= 1 << from_sq
+            occ |= 1 << to_sq
+
+            color_white = not side_white
+            depth = 1
+            while True:
+                atk_mask = attackers_to_square(to_sq, occ, color_white, removed_mask)
+                if atk_mask == 0:
+                    break
+                # choose least valuable attacker
+                best_sq = -1
+                best_val = 10**9
+                best_piece = None
+                mask = atk_mask
+                while mask:
+                    lsb = mask & -mask
+                    sq = lsb.bit_length() - 1
+                    p = piece_on_square(sq)
+                    if p is not None:
+                        val = piece_vals[p]
+                        if val < best_val:
+                            best_val = val
+                            best_sq = sq
+                            best_piece = p
+                    mask ^= lsb
+                if best_sq == -1 or best_piece is None:
+                    break
+                # forward recurrence
+                gain.append(best_val - gain[-1])
+                # update occ: remove attacker from its square; target stays occupied
+                occ &= ~(1 << best_sq)
+                removed_mask |= 1 << best_sq
+                color_white = not color_white
+                depth += 1
+
+            # Backward propagation to compute optimal outcome
+            for i in range(len(gain) - 2, -1, -1):
+                gain[i] = max(-gain[i], gain[i + 1])
+            return gain[0]
+
         def negamax(d: int, alpha: int, beta: int, ply: int = 0) -> Tuple[int, List[Move]]:
             nonlocal nodes
             nodes += 1
@@ -229,11 +440,7 @@ class SearchService:
                     board.ep_square is not None and m.to_sq == board.ep_square
                 )
                 if is_capture and d <= 2:
-                    att = attacker_piece_index(m)
-                    vic = victim_piece_index(m)
-                    v = piece_vals[vic] if vic is not None else 0
-                    a = piece_vals[att] if att is not None else 0
-                    if (v - a) < 0:
+                    if see(m) < 0:
                         continue
                 board.make_move(m)
                 child_score, child_pv = negamax(d - 1, -beta, -alpha, ply + 1)
